61be4a9788dd5c8e4fe2c3b0095dc5731fd2fa21
Karisue1 <kmora048@fiu.edu>
2024-09-13 11:33:22 -0400

    changed the logo

--- FrontendBudgetMunch/budgemunchfrontend/src/layout/Navbar.js
+++ FrontendBudgetMunch/budgemunchfrontend/src/layout/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success" style={{ fontFamily: 'Playwrite AU NSW, sans-serif' }}>
   <div className="container-fluid">
-    <img src="/Budget.png" alt="BudgetMunch Logo" width="100" height="100" className="d-inline-block align-text-top" />
+    <img src="/Logo.jpg" alt="BudgetMunch Logo" width="100" height="100" className="d-inline-block align-text-top" />
       <a className="navbar-brand" href="#">BudgetMunch</a>
         <button className="navbar-toggler"
                 type="button"
