47475d671f7f959894ad29e3f84b6a112a735576
Karisue1 <kmora048@fiu.edu>
2024-09-13 13:17:08 -0400

    adding new image

--- FrontendBudgetMunch/budgemunchfrontend/src/layout/Navbar.js
+++ FrontendBudgetMunch/budgemunchfrontend/src/layout/Navbar.js
@@ -5,7 +5,8 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success" style={{ fontFamily: 'Playwrite AU NSW, sans-serif' }}>
   <div className="container-fluid">
-      <a className="navbar-brand" href="#">BudgetHelloMunch</a>
+    <img src="/logo small.png" alt="BudgetMunch Logo" width="100" height="100" className="d-inline-block align-text-top" />
+      <a className="navbar-brand" href="#">BudgetMunch</a>
         <button className="navbar-toggler"
                 type="button"
                 data-bs-toggle="collapse"
