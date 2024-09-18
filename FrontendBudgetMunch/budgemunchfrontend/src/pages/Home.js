eebbf1dc33e7a660253cf3d1fd83c351ebcdc65f
Trevor <tsimp010@fiu.edu>
2024-09-17 21:33:40 -0400

    localhost3.0

--- FrontendBudgetMunch/budgemunchfrontend/src/pages/Home.js
+++ FrontendBudgetMunch/budgemunchfrontend/src/pages/Home.js
@@ -2,46 +2,45 @@ import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
 export default function Home() {
-const [students, setStudents] = useState([])
+  const [restaurants, setRestaurants] = useState([])
 
-useEffect(()=>{
- loadStudents();
-},[]);
+  useEffect(() => {
+    loadRestaurants();
+  }, []);
 
-const loadStudents =async ()=>{
- const result=await axios.get("http://localhost:8080/api/v1/budget/getLocation");
-  setStudents(result.data);
-}
+  const loadRestaurants = async () => {
+    const result = await axios.get("http://localhost:8080/api/v1/budget/getLocation");
+    setRestaurants(result.data);
+  }
 
   return (
     <div className="container">
-        <div className="py-4">
+      <div className="py-4">
         <table className="table table-bordered shadow">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Age</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    {
-      students.map((student,index)=>(
-        <tr>
-      <th scope="row" key={index}>{index+1}</th>
-      <td>{student.name}</td>
-      <td>{student.email}</td>
-      <td>{student.age}</td>
-    </tr>
-      ))
-    }
-    
-    
-  </tbody>
-</table>
-        </div>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Vicinity</th>
+              <th scope="col">Rating</th>
+              <th scope="col">Price Level</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              restaurants.map((restaurant, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{restaurant.name}</td>
+                  <td>{restaurant.vicinity}</td>
+                  <td>{restaurant.rating}</td>
+                  <td>{restaurant.price_level}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
