ec2080cea99ce730b151707ccf6b1a8f5bb39300
Trevor <tsimp010@fiu.edu>
2024-09-17 21:29:47 -0400

    localhost

--- FrontendBudgetMunch/budgemunchfrontend/src/pages/Home.js
+++ FrontendBudgetMunch/budgemunchfrontend/src/pages/Home.js
@@ -4,14 +4,14 @@ import axios from "axios";
 export default function Home() {
 const [students, setStudents] = useState([])
 
-//useEffect(()=>{
-//  loadStudents();
-//},[]);
+useEffect(()=>{
+ loadStudents();
+},[]);
 
-//const loadStudents =async ()=>{
- // const result=await axios.get("localhost");
-  //setStudents(result.data);
-//}
+const loadStudents =async ()=>{
+ const result=await axios.get("http://localhost:8080/api/v1/budget/getLocation");
+  setStudents(result.data);
+}
 
   return (
     <div className="container">
